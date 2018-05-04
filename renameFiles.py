83fcacd517cf2078d4fa75cec2a3f5947f9f0009
jin <Jin Zhu>
2018-05-04 16:21:51 -0400

    rename file edited

--- renameFiles.py
+++ renameFiles.py
@@ -1,5 +1,5 @@
 import os
-imgdir = "./input_imgs/"
+imgdir = "./training_data/fifth_dataset/images_may4/"
 for filename in os.listdir(imgdir):
     newname = filename.replace('rotatedroadframe', '')
     os.rename(imgdir+filename, imgdir+newname)
